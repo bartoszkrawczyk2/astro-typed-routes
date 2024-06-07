b396041960aabe5f9e1405f762c01a6c568f28fc
Bartosz Krawczyk <bartoszkrawczyk2@gmail.com>
2024-06-07 09:21:30 +0200

    add tests for static pages

--- src/typegen.test.ts
+++ src/typegen.test.ts
@@ -1,8 +1,53 @@
 import assert from "node:assert";
 import { describe, it } from "node:test";
+import { generateTypeString } from "./typegen";
+import type { Dirent } from "node:fs";
 
-describe("tests", () => {
-  it("passes", () => {
-    assert.ok(true);
+const mock = {
+  file: (path: string, name: string) =>
+    ({
+      isFile: () => true,
+      path,
+      name,
+    } as Dirent),
+
+  dir: (path: string) =>
+    ({
+      isFile: () => false,
+      path,
+    } as Dirent),
+};
+
+describe("static pages", () => {
+  it("removes index from route", () => {
+    const result = generateTypeString("/root", [
+      mock.file("/root", "index.astro"),
+      mock.file("/root/api/nested", "index.ts"),
+    ]);
+    assert.strictEqual(
+      result,
+      'type AstroRoutes = [route: "/"] |\n[route: "/api/nested"];'
+    );
+  });
+
+  it("ignore dirents that are not files", () => {
+    const result = generateTypeString("/root", [
+      mock.dir("/root"),
+      mock.file("/root", "index.astro"),
+    ]);
+    assert.strictEqual(result, 'type AstroRoutes = [route: "/"];');
+  });
+
+  it("handles file names other than index", () => {
+    const result = generateTypeString("/root", [
+      mock.file("/root", "page.astro"),
+      mock.file("/root/nested", "other-page.astro"),
+    ]);
+    assert.strictEqual(
+      result,
+      `type AstroRoutes = [route: "/page"] |\n[route: "/nested/other-page"];`
+    );
   });
 });
+
+describe("dynamic pages", () => {});
